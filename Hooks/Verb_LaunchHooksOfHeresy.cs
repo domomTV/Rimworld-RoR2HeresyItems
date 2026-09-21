e596d527b4da44dd9cded81b57f50c13774c4e9e
domom <domomiguel04@gmail.com>
2026-09-20 20:18:15 -0400

    Organized files
    - Organized files by ability
    - Seperated Verb_LaunchHooksOfHeresy from Verb_LaunchProjectileHeresy.cs
    - Gitignore no longer ignores all folders

---
+++ Hooks/Verb_LaunchHooksOfHeresy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+// Fixes wrong rulepack being used for battle log
+public class Verb_LaunchHooksOfHeresy : Verb_LaunchProjectileHeresy {
+	
+	public override void WarmupComplete() {
+		// Stolen from Verb
+		this.burstShotsLeft = this.ShotsPerBurst;
+		this.state = VerbState.Bursting;
+		this.TryCastNextBurstShot();
+		
+		// Stolen & Changed from Verb_LaunchProjectile
+		Find.BattleLog.Add((LogEntry) new BattleLogEntry_RangedFire(this.caster, this.currentTarget.HasThing ? this.currentTarget.Thing : (Thing) null, ThingDef.Named("domom_Hooks_VerbScum"), this.Projectile, this.ShotsPerBurst > 1));
+	}
+	
+}
--- Verb_LaunchProjectileHeresy.cs
+++ Verb_LaunchProjectileHeresy.cs
@@ -33,17 +33,3 @@ public class Verb_LaunchProjectileHeresy : Verb_LaunchProjectileStatic, IAbility
 	}
 }
 
-// Fixes wrong rulepack being used for battle log
-public class Verb_LaunchHooksOfHeresy : Verb_LaunchProjectileHeresy {
-	
-	public override void WarmupComplete() {
-		// Stolen from Verb
-		this.burstShotsLeft = this.ShotsPerBurst;
-		this.state = VerbState.Bursting;
-		this.TryCastNextBurstShot();
-		
-		// Stolen & Changed from Verb_LaunchProjectile
-		Find.BattleLog.Add((LogEntry) new BattleLogEntry_RangedFire(this.caster, this.currentTarget.HasThing ? this.currentTarget.Thing : (Thing) null, ThingDef.Named("domom_Hooks_VerbScum"), this.Projectile, this.ShotsPerBurst > 1));
-	}
-	
-}
